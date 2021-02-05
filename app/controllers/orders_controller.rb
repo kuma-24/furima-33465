cb86d28f8ba5432a204bbacb44ed8776013d1d1a
github user name <surei.mikurio2525@gmail.com>
2021-02-05 15:22:30 +0900

    配送先情報実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,34 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
+    @order_form = OrderForm.new
   end
 
-  def new
+  def create
+    @order_form = OrderForm.new(order_params)   
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order_form).permit(
+      :postal_code, 
+      :city, 
+      :address, 
+      :building, 
+      :phone_number, 
+      :delivery_prefecture_id
+    ).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
